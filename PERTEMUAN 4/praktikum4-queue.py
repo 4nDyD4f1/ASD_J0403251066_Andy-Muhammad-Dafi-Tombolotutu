f54e1b8f16b34cab6e981a2ef984e0797a40337d
4nDyD4f1 <andydafi407@gmail.com>
2026-02-18 16:08:29 +0700

    Refactor QueueLL class: add is_empty method and clean up enqueue logic

--- PERTEMUAN 4/praktikum4-queue.py
+++ PERTEMUAN 4/praktikum4-queue.py
@@ -8,12 +8,15 @@ class QueueLL:
         self.front = None #node paling depan
         self.rear = None  #node paling belakang
 
+    def is_empty(self):
+        return self.front is None
+
     def enqueue(self,data):
         #Menambahkan data di belakang (rear)
         nodeBaru = Node(data)
 
         #jika queue kosong, front dan rear menunjukkan kode yang sama
-        if self.is_empty(self):
+        if self.is_empty():
             self.front = nodeBaru
             self.rear = nodeBaru
             return
@@ -27,15 +30,15 @@ class QueueLL:
     def tampilkan(self):
 
         current = self.front
-        print("Front -> ", end="->")
+        print("Front ->", end="")
         while current is not None:
             print(current.data, end="->")
             current = current.next
         print("None - Rear di node terakhir")
 
-    #Intantiasi objek class QueueLL
-    q = QueueLL()
-    q.enqueue("A")
-    q.enqueue("B")
-    q.enqueue("C")
-    q.tampilkan()
+# Intantiasi objek class QueueLL
+q = QueueLL()
+q.enqueue("A")
+q.enqueue("B")
+q.enqueue("C")
+q.tampilkan()
